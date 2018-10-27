538c9d05a38b115e2eabc64ff327c89855b87b9b
Brandon Hao <brandon.hao@outlook.com>
2018-10-26 21:00:40 -0400

    Base app
    Functioning prototype/framework

--- LegoFramework/app/src/main/java/com/example/brand/legoframework/ConnectionActivity.java
+++ LegoFramework/app/src/main/java/com/example/brand/legoframework/ConnectionActivity.java
@@ -1,22 +1,53 @@
 package com.example.brand.legoframework;
 
 import android.bluetooth.BluetoothAdapter;
-import android.support.v7.app.AppCompatActivity;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
-public class ConnectionActivity extends AppCompatActivity {
-    private BluetoothAdapter bAdapter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Set;
 
+//Java class (tentatively)complete
+//TODO: Create user friendly UI - implement custom action bar for app nav
+public class ConnectionActivity extends AppCompatActivity {
+    private Set<BluetoothDevice> devices;
+    private BluetoothDevice selectedDevice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
-        bAdapter = BluetoothAdapter.getDefaultAdapter();
+        BluetoothAdapter bAdapter = BluetoothAdapter.getDefaultAdapter();
+        devices = bAdapter.getBondedDevices();
     }
 
     public void onClick (View view) {
         String address = ((EditText)findViewById(R.id.Mac_Add)).getText().toString();
+        for (BluetoothDevice b :
+                devices) {
+            if(b.getAddress().equals(address)) {
+                selectedDevice = b;
+            }
+        }
+        try {
+            FileOutputStream fileOut;
+            int length = selectedDevice.getName().getBytes().length + selectedDevice.getAddress().length() + 1;
+            fileOut = openFileOutput(getResources().getString(R.string.current_device), 0);
+            fileOut.write(length);
+            fileOut.write(32);
+            fileOut.write(selectedDevice.getName().getBytes());
+            fileOut.write(32);
+            fileOut.write(selectedDevice.getAddress().getBytes());
+            fileOut.close();
+
+        }
+        catch(IOException e) {
+            Log.d("IO ERROR", "Failed to write data file.");
+            e.printStackTrace();
+        }
     }
 }
--- LegoFramework/app/src/main/java/com/example/brand/legoframework/MainActivity.java
+++ LegoFramework/app/src/main/java/com/example/brand/legoframework/MainActivity.java
@@ -3,21 +3,28 @@ package com.example.brand.legoframework;
 import android.annotation.TargetApi;
 import android.app.TaskStackBuilder;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+//Prototype/framework for the app. Complies and runs but it doesn't really do anything as of now
+//This activity is more or less complete
+//TODO: Check exit ops and program shutdown procedure
+//TODO: Improve UI - custom action bar
 public class MainActivity extends AppCompatActivity {
-    public static BluetoothDevice bDevice;
+    public static String DEVICE_KEY = "Device_DAT";
+    private TaskStackBuilder screenStack;
     private final int REQUEST_ENABLE_BT = 1;
     private final int REQUEST_ENABLE_FINE_LOCATION = 2;
     private final int REQUEST_ENABLE_COARSE_LOCATION = 3;
@@ -66,10 +73,14 @@ public class MainActivity extends AppCompatActivity {
     }
     //endregion
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        screenStack = TaskStackBuilder.create(MainActivity.this);
+        screenStack.addNextIntent(new Intent(MainActivity.this, MainActivity.class));
         checkPermissions();
         if(!BluetoothAdapter.getDefaultAdapter().isEnabled()) {
             Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
@@ -84,18 +95,34 @@ public class MainActivity extends AppCompatActivity {
         switch (source) {
             case "Start":
                 //TODO: Implement game
+                try{
+                    InputStream fileInput;
+                    String fileName = getResources().getString(R.string.current_device);
+                    StringBuilder dataIn = new StringBuilder();
+                    fileInput = openFileInput(fileName);
+                    while(fileInput.available() > 0){
+                        dataIn.append(fileInput.read());
+                    }
+                    Intent gameIntent = new Intent(MainActivity.this, GameActivity.class);
+                    gameIntent.putExtra(DEVICE_KEY, dataIn.toString());
+                    screenStack.addNextIntent(gameIntent);
+                    screenStack.startActivities();
+                }
+                catch (IOException e){
+                    Toast.makeText(this, "Data file not found! Use connect function to select a device.", Toast.LENGTH_LONG).show();
+                }
                 break;
 
             case "Connect":
-                //TODO: Interface with connection screen
                 Intent connectIntent = new Intent(MainActivity.this, ConnectionActivity.class);
-                TaskStackBuilder screenStack = TaskStackBuilder.create(MainActivity.this);
-                screenStack.addNextIntent(new Intent (MainActivity.this, MainActivity.class));
                 screenStack.addNextIntent(connectIntent);
+                screenStack.startActivities();
                 break;
 
             case "Exit":
                 //TODO: Implement exit ops
+                finish();
+                System.exit(0);
                 break;
         }
     }
